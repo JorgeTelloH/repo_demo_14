16feaaf605cd11049fcb1478c0fcaf14aa44151c
JorgeTelloH <jtello@cabalcon.com.pe>
2022-07-21 23:29:59 -0500

    Conductor en Empleados

--- hr_employee_driver/__manifest__.py
+++ hr_employee_driver/__manifest__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Agregar atributos de conductor al personal",
+    'name': "Conductores en Empleados",
 
     'summary': """
-        Adiciona datos de conductor al personal""",
+        Agrega Datos de Conductor en Empleados""",
 
     'description': """
-        Adiciona nuevos atributos de Conductor al personal: Datos de Conductor, si es de planta o externo, entre otros.
+        Agrega Datos de Conductor en Empleados.
     """,
 
     'author': "TH",
     'website': "http://www.cabalcon.com",
 
-    'category': 'Human Resources',
+    'category': 'Human Resources/Employees',
     'version': '1.1',
 
     # any module necessary for this one to work correctly
@@ -22,6 +22,8 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/hr_employee_view.xml',
+        'views/hr_type_driver_license_view.xml',
+        'data/data_type_driver_license.xml',
     ],
 
 }
--- hr_employee_driver/models/hr_employee.py
+++ hr_employee_driver/models/hr_employee.py
@@ -1,27 +1,58 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 from datetime import datetime
 
+class HrTypeDriverLicense(models.Model):
+    _name = 'hr.type.driver.license'
+    _description = 'Tipos de Licencia de Conducir'
+
+    name = fields.Char(string='Tipo de Licencia', required=True)
+    active = fields.Boolean(string='Activo', default=True)
+
+
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    driver = fields.Boolean(string='Es Conductor?', help='Usado para definir si esta persona es un conductor')
-    type_employee = fields.Selection([("planilla", "Planilla"), ("tercero", "Tercero")], string='Tipo de Colaborador (No usar)', default='planilla')
-    partner_id = fields.Many2one('res.partner', string='Proveedor', index=True)
-    driver_license = fields.Char(string='Nro Licencia')
-    license_type = fields.Char(string='Tipo de Licencia')
-    license_expiration = fields.Date(string='Fecha caducidad')
-    days_to_expire = fields.Integer(compute='_compute_days_to_expire', string='Días de caducidad')
+    driver = fields.Boolean(string='Es Conductor?', help='Activar solo si el Empleado es conductor')
+    type_employee = fields.Selection([("planilla", "Planilla"), ("tercero", "Tercero")], string='Tipo Empleado (No Usar)', default='planilla')
+    partner_id = fields.Many2one('res.partner', string='Proveedor (No Usar)', index=True)
+    driver_license = fields.Char(string='Nro Brevete')
+    license_type = fields.Char(string='Tipo Licencia (No Usar)')
+    license_type_id = fields.Many2one('hr.type.driver.license', string="Tipo de Brevete")
+    license_expiration = fields.Date(string='Caducidad de Brevete', help='Fecha de Caducidad de Brevete')
+    days_to_expire = fields.Integer(compute='_compute_days_to_expire', string='Caducidad de Brevete')
+    #SOAT
+    soat_nbr = fields.Char(string='Nro SOAT')
+    soat_start_date = fields.Date(string='Fecha Desde')
+    soat_finish_date = fields.Date(string='Fecha Hasta', help='Fecha de Caducidad de SOAT')
+    soat_days_to_expire = fields.Integer(compute='_compute_soat_days_to_expire', string='Caducidad de SOAT')
+
 
     @api.depends('license_expiration')
     def _compute_days_to_expire(self):
         for rec in self:
-            now = fields.Date().today()
-            date_expire = rec.license_expiration or fields.Date().today()
-            delta = date_expire - now
-            if delta.days >= -1:
-                rec.days_to_expire = delta.days + 1
+            if rec.license_expiration:
+                now = fields.Date().today()
+                date_expire = rec.license_expiration or fields.Date().today()
+                delta = date_expire - now
+                if delta.days >= -1:
+                    rec.days_to_expire = delta.days + 1
+                else:
+                    rec.days_to_expire = 0
             else:
                 rec.days_to_expire = 0
+
+    @api.depends('soat_finish_date')
+    def _compute_soat_days_to_expire(self):
+        for rec in self:
+            if rec.soat_finish_date:
+                now = fields.Date().today()
+                soat_date_expire = rec.soat_finish_date or fields.Date().today()
+                delta = soat_date_expire - now
+                if delta.days >= -1:
+                    rec.soat_days_to_expire = delta.days + 1
+                else:
+                    rec.soat_days_to_expire = 0
+            else:
+                rec.soat_days_to_expire = 0
