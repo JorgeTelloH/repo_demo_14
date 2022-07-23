24c7ac8d1bfca13caa4a7ddcb07ec1f6deb838e4
JorgeTelloH <jtello@cabalcon.com.pe>
2022-07-22 20:54:15 -0500

    Empleados

--- cabalcon_hr/models/res_company.py
+++ cabalcon_hr/models/res_company.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    essalud_tax = fields.Float(string='EsSalud tasa')
-    sctr_tax = fields.Float(string='SCTR tasa')
+    essalud_tax = fields.Float(string='EsSalud Tasa')
+    sctr_tax = fields.Float(string='SCTR Tasa')
     uit_tax = fields.Float(string='UIT tasa')
     eps_tax = fields.Float(string='EPS %')
 
@@ -14,25 +14,17 @@ class ResCompany(models.Model):
     sctr_rmv = fields.Float(string='SCTR RMV')
     uit_rmv = fields.Float(string='UIT RMV')
 
-    rubro = fields.Char(string='Rubro')
+    rubro = fields.Char(string='Rubro de la Compañia')
     # campo necesario para el modelo de liquidacion CTS
     general_manager_id = fields.Many2one('res.partner', string='Gerente General', help="Gerente General de la Compañia")
     product_id = fields.Many2one('product.product', string='Producto Recibo por Honorarios',
                                  domain="[('type', '=', 'service'), '|', ('company_id', '=', False), ('company_id', '=', id)]")
-    insurance_premium_cap = fields.Float(string='Tope de prima de seguros')
+    insurance_premium_cap = fields.Float(string='Tope de prima de Seguros')
     eps_credit_amount = fields.Monetary(string='Importe crédito EPS')
     ofic_norm_prev = fields.Float(string='ONP')
 
-    sector = fields.Selection(string='Sector',
-                              selection=[('PRIVS', 'Sector Privado'),
-                                         ('PUBS', 'Sector Publico'),
-                                         ('OE', 'Otras Entidades')])
-
-    company_type = fields.Selection(string='Tipo de compañia',
-                                    selection=[('01', 'Empresa'),
-                                               ('02', 'MIPYMES')],
-                                    default='01')
     sigla = fields.Char(string='Sigla')
+    #Adelantos
     account_type = fields.Selection(string='Tipo de Cuenta Cargo',
                                     selection=[('C', 'Corriente'),
                                                ('M', 'Maestra')],
